da5324d3d8cfa1671722a1d1dadbdfb6459ee620
yusuf <horiyorrmi72@gmail.com>
2025-03-23 10:00:02 +0100

    fix(conflict): clear auth service conflicts

--- src/app/admin/service/auth.service.ts
+++ src/app/admin/service/auth.service.ts
@@ -2,27 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router'; // Ensure Router is imported
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private baseUrl: string = environment.apiBaseUrl;
-  constructor(private http:HttpClient) { }
 
-  
+  constructor(private http: HttpClient, private router: Router) { }
 
   signIn(userData: any): Observable<any> {
     return this.http.post(`${this.baseUrl}/auth/login`, userData);
   }
 
-  isUserLoggedIn():boolean{
-    return localStorage.getItem('token') != null;
+  isUserLoggedIn(): boolean {
+    return localStorage.getItem('token') !== null;
   }
 
-
-  signOut(): void{
-    return localStorage.removeItem('token');
+  signOut(): void {
+    localStorage.removeItem('token');
+    this.router.navigate(['/admin']);
   }
-
 }
